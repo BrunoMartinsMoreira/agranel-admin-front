24b0fdccc36f858bbab91d4f83da9942a12368c9
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2023-03-13 08:09:56 -0300

    feat: pagina de cadastrar-se criada

---
+++ src/Pages/Register/RegisterPage.tsx
@@ -0,0 +1,76 @@
+import { Flex, Button, Stack, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Form/Input';
+
+export const RegisterPage = () => {
+  return (
+    <Flex w='100vw' h='100vh' alignItems='center' justifyContent='center'>
+      <Flex
+        as='form'
+        flexDirection='column'
+        width='100%'
+        maxWidth={600}
+        bg='gray.800'
+        p='8'
+        borderRadius={8}
+      >
+        <Text as='b' color='gray.100' fontSize='36px' pt='4' mb='8'>
+          CADASTRAR-SE
+        </Text>
+        <Stack spacing='4'>
+          <Input
+            name='name'
+            type='text'
+            label='Nome'
+            placeholder='Nome'
+            /* error={errors.email}
+        {...register('name')}*/
+          />
+          <Input
+            name='email'
+            type='email'
+            label='E-mail'
+            placeholder='E-mail'
+            /* error={errors.email}
+        {...register('email')}*/
+          />
+          <Input
+            name='password'
+            type='password'
+            label='Senha'
+            placeholder='Senha'
+            /* error={errors.password}
+        {...register('password')}*/
+          />
+          <Button
+            type='submit'
+            mt='6'
+            colorScheme='green'
+            size='lg'
+            /* isLoading={formState.isSubmitting}*/
+          >
+            Salvar
+          </Button>
+        </Stack>
+        <Flex p='3' pt='8' width='100%' justify='space-between'>
+          <Text size='8'>Já possui Conta?</Text>
+          <Link to='/'>
+            <Text
+              as='b'
+              color='blue.500'
+              border='2px solid transparent'
+              borderRadius='5px'
+              transition='.3s ease-in-out'
+              padding='2'
+              _hover={{
+                border: '2px solid blue',
+              }}
+            >
+              LOGIN
+            </Text>
+          </Link>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from '../Pages/Login/LoginPage';
+import { RegisterPage } from '../Pages/Register/RegisterPage';
 // import { Products } from '../components/Products/Products';
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<LoginPage />} />
-      <Route path='/dashboard' element={<h1>dashboard page</h1>} />
+      <Route path='/register' element={<RegisterPage />} />
     </Routes>
   );
 };
