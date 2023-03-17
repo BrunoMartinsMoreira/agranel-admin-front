6624060208515ecb29a6a05a46dcee981089d246
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2023-03-16 21:30:22 -0300

    feat: add rotas de login e produtos

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import { Dashboard } from '../Pages/Dashboad/Dashboard';
 import { LoginPage } from '../Pages/Login/LoginPage';
+import { ProductsPage } from '../Pages/Products/ProductsPage';
 import { RegisterPage } from '../Pages/Register/RegisterPage';
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Dashboard />} />
+      <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
+      <Route path='/products' element={<ProductsPage />} />
     </Routes>
   );
 };
