d66ab79c1ad453ee7bf28c54ebb7afd9f8011982
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2023-03-18 23:31:11 -0300

    feat: add navegação

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,30 +1,32 @@
-import { Link, Text } from '@chakra-ui/react';
+import { Link as UILink, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export function Logo() {
   return (
-    <Link
-      _hover={{
-        borderBottom: 'none',
-      }}
-      href='/'
-    >
-      <Text
-        fontSize={['4xl', '5xl']}
-        fontWeight='500'
-        letterSpacing='tight'
-        w={['45', '64']}
-        fontFamily='Cookie'
-        color='green.600'
-        px='2'
-        py='2'
-        textAlign='center'
-        mr={['0', '10']}
+    <Link to='/'>
+      <UILink
+        _hover={{
+          borderBottom: 'none',
+        }}
       >
-        Agranel
-        <Text as='span' ml='1' color='pink.400' fontFamily='Cookie'>
-          Ita
+        <Text
+          fontSize={['4xl', '5xl']}
+          fontWeight='500'
+          letterSpacing='tight'
+          w={['45', '64']}
+          fontFamily='Cookie'
+          color='green.600'
+          px='2'
+          py='2'
+          textAlign='center'
+          mr={['0', '10']}
+        >
+          Agranel
+          <Text as='span' ml='1' color='pink.400' fontFamily='Cookie'>
+            Ita
+          </Text>
         </Text>
-      </Text>
+      </UILink>
     </Link>
   );
 }
